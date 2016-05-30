f6f745fbfae1d8dcb435aa1cd5679c9ce9f600d0
Nick Lalic <nick.lalic@gmail.com>
2016-05-30 12:16:46 -0700

    Fixed Visitors#Index error that occurred when no user was logged in

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,9 @@
 class VisitorsController < ApplicationController
   def index
     @user = current_user
-    @new_url = @user.short_urls.new
     @short_urls = nil
     if @user
+      @new_url = @user.short_urls.new
       @short_urls = @user.short_urls.order('created_at desc').paginate(page: params[:page])
     end
   end
