8382eb19488c29b90f868558579faad38fa279b5
Nick Lalic <nick.lalic@gmail.com>
2016-05-29 19:06:34 -0700

    Creating home page for logged in users with a form for creating new urls and a list of existing urls created by the user.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,10 @@
 class VisitorsController < ApplicationController
+  def index
+    @user = current_user
+    @new_url = @user.short_urls.new
+    @short_urls = nil
+    if @user
+      @short_urls = @user.short_urls.order('created_at desc').paginate(page: params[:page])
+    end
+  end
 end
