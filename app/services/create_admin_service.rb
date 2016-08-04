45c4875be3bbc547b21761966a4f3afd7d6e7d85
Nick Lalic <nick.lalic@gmail.com>
2016-08-04 03:49:55 -0700

    Fixed create_admin_service: user.confirm- removed exclamation point.

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -4,7 +4,7 @@ class CreateAdminService
         user.name                  = Rails.application.secrets.admin_name
         user.password              = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
-        user.confirm!
+        user.confirm
       end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160702063810) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "short_urls", force: :cascade do |t|
     t.string   "url"
     t.integer  "user_id"
@@ -21,8 +24,8 @@ ActiveRecord::Schema.define(version: 20160702063810) do
     t.string   "title"
   end
 
-  add_index "short_urls", ["user_id", "created_at"], name: "index_short_urls_on_user_id_and_created_at"
-  add_index "short_urls", ["user_id"], name: "index_short_urls_on_user_id"
+  add_index "short_urls", ["user_id", "created_at"], name: "index_short_urls_on_user_id_and_created_at", using: :btree
+  add_index "short_urls", ["user_id"], name: "index_short_urls_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(version: 20160702063810) do
     t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
